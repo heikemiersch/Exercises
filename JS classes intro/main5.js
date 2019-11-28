a8f4e8bea187e5df9c49ec967cd5fdfcf1585f94
Heike Miersch <heike.miersch@hotmail.de>
2019-11-28 22:58:18 +0100

    found more ways that do not work

--- JS classes intro/main5.js
+++ JS classes intro/main5.js
@@ -20,23 +20,30 @@ let p3 = new Product("bike", 300);
 let p4 = new Product("shoes", 45);
 let p5 = new Product("hoodie", 30);
 
-// let totalPrice = p1.price + p2.price + p3.price;
-// let totalPrice = p1.price + p2.price + p3.price + p4.price + p5.price;
-// let totalPrice = [];
-
 // Now, let's create a ShoppingCart class, that will contain products
 
 class ShoppingCart {
   constructor() {
     this.products = []; // empty at first
     this.total = 0;
-    this.repeatingItems = 0;
-    console.log(this.repeatingItems);
+    this.amount = {};
+    // object contains product.name and product.amount
   }
 
   addProduct(product) {
     this.products.push(product);
     // console.log(this.products);
+  }
+
+  increaseAmount() {
+    // if a product is added, amount goes to 1, if another one of same kind is added,
+    //  don't add product but set amount to += amount, being plus one
+    if (this.amount == undefined) {
+      this.amount = 1;
+    } else {
+      this.amount += this.amount;
+    }
+    return this.amount;
   }
 
   toString() {
@@ -47,41 +54,52 @@ class ShoppingCart {
     for (let i = 0; i < this.products.length; i++) {
       this.total += this.products[i].price;
     }
-    if (cart.products.length >= 5) {
+    if (this.products.length >= 5) {
       return "total price is " + (this.total / 10) * 9 + " €";
     } else {
       return "total price is " + this.total + " €";
     }
   }
 
-  discountOnEveryFourthItem() {
-    // for (let i = 0; i < this.products.length; i++) {
-    // 	for (let j = this.products.length; j >= i; j--) {
-    // 		if (i !== j && this.products[i].name == this.products[j].name) {
-    // 			this.repeatingItems.push(this.products[i]);
-    // 		}
-    // 	}
-    // }
-    // return this.repeatingItems;
-    // for (let i = 0; i < this.products.length; i++) {
-    // 	if ((i % 4 >= 1) && (this.products.name[i] === this.products.name[i])) {
-    // 	}
-    // }
-    // console.log(this.products[0].name);
-    // oder wenn die summe aller gleichen items restlos durch 4 teilbar ist, dann bitte ergebnis ermitteln
-    // und bis auf ohne kommastellen abrunden und so viele items nicht bepreisen
-  }
+  // determineDuplicates() {
+  //   for (let i = 0; i < this.products.length; i++) {
+  //     for (let j = this.products.length - 1; j >= i; j--) {
+  //       // console.log(this.products[i].name)
+  //       if (i !== j && this.products[i].name == this.products[j].name) {
+  //         this.repeatingItems.push(this.products[i].name);
+  //       }
+  //     }
+  //   }
+  //   return this.repeatingItems;
+  // }
+
+  // problem here: method pushes exponential amount into the array.  
+  // so adding products to the cart, results in 1 3 6 10 15 21
+
+  // discountOnEveryFourthItem() {
+  //   for (let i = 0; i < this.products.length; i++) {
+  //     if ((i / 4 >= 1) && (this.products.name[i] === this.products.name[i])) {}
+  //   }
+  // console.log(this.products[0].name);
+  // or if the sum of all repeating items is divisible by four, determine result and mathfloor it,
+  // then multiply new result by price
+  // }
 }
 
 // We can create a cart and add the products we created before
 
 let cart = new ShoppingCart();
+
+cart.addProduct(p1);
+cart.addProduct(p1);
+cart.addProduct(p1);
 cart.addProduct(p1);
 cart.addProduct(p2);
 cart.addProduct(p3);
+cart.addProduct(p3);
+cart.addProduct(p3);
+cart.addProduct(p3);
 cart.addProduct(p4);
-cart.addProduct(p5);
-cart.addProduct(p5);
 cart.addProduct(p5);
 
 console.log("We have a " + cart);
@@ -91,7 +109,10 @@ console.log(cart.products[0].price);
 
 console.log(cart.totalPrice());
 console.log(cart.products.length);
-console.log(cart.products[5].name);
+console.log(cart.products[3].name);
 console.log(cart.products[3].price);
 
-console.log(cart.repeatingItems);
+cart.increaseAmount();
+console.log(cart.amount);
+
+console.log(cart.total);
